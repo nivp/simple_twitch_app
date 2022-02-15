069f1ed541a9963caf8f8e2d33bd758dee8d462c
nivp <nivpld@gmail.com>
2022-02-15 23:16:38 +0200

    async startup

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const { ElectronChromeExtensions } = require('electron-chrome-extensions')
 require('@electron/remote/main').initialize()
 const buildChromeContextMenu = require('electron-chrome-context-menu').default
 
-
-const createWindow = () => {
-
+app.on('ready', () => {
     const win = new BrowserWindow({
         title: "Simple Twitch App",
         width: 1440,
@@ -21,54 +19,55 @@ const createWindow = () => {
         icon: __dirname + "/twitch.png",
     })
 
-    const extensions = new ElectronChromeExtensions({
-        session: session.defaultSession,
-        createTab(details) {
-            // Optionally implemented for chrome.tabs.create support
-        },
-        selectTab(tab, browserWindow) {
-            // Optionally implemented for chrome.tabs.update support
-        },
-        removeTab(tab, browserWindow) {
-            // Optionally implemented for chrome.tabs.remove support
-        },
-        createWindow(details) {
-            // Optionally implemented for chrome.windows.create support
-        },
-    })
-
-    extensions.addTab(win.webContents, win)
-
     require('@electron/remote/main').enable(win.webContents)
 
-    session.defaultSession.loadExtension(__dirname + '\\extensions\\BetterTTV')
-    session.defaultSession.loadExtension(__dirname + '\\extensions\\Video-Ad-Block--for-Twitch')
-    session.defaultSession.loadExtension(__dirname + '\\extensions\\ublock_origin', { allowFileAccess: true })
+    app.on('web-contents-created', async (event, webContents) => {
+        const extensions = new ElectronChromeExtensions({
+            session: session.defaultSession,
+            createTab(details) {
+                // Optionally implemented for chrome.tabs.create support
+            },
+            selectTab(tab, browserWindow) {
+                // Optionally implemented for chrome.tabs.update support
+            },
+            removeTab(tab, browserWindow) {
+                // Optionally implemented for chrome.tabs.remove support
+            },
+            createWindow(details) {
+                // Optionally implemented for chrome.windows.create support
+            },
+        })
+    
+        extensions.addTab(win.webContents, win)
+
+        session.defaultSession.loadExtension(__dirname + '\\extensions\\BetterTTV')
+        session.defaultSession.loadExtension(__dirname + '\\extensions\\Video-Ad-Block--for-Twitch')
+
+
+        if (webContents.getType() === 'webview') {
+            webContents.setWindowOpenHandler(({ url }) => {
+                require('electron').shell.openExternal(url)
+                return { action: 'deny' }
+            })
+        }
+
+        webContents.on('context-menu', async (e, params) => {
+            const menu = buildChromeContextMenu({
+                params,
+                webContents,
+                openLink: (url, disposition) => {
+                    webContents.loadURL(url)
+                }
+            })
+            session.defaultSession.loadExtension(__dirname + '\\extensions\\ublock_origin', { allowFileAccess: true })
+
+            menu.popup()
+        })
+    })
+
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') app.quit()
+    })
 
     win.loadURL(`file://${__dirname}/twitch.html`)
-}
-
-app.whenReady().then(() => {
-    createWindow()
-})
-
-app.on('web-contents-created', (event, webContents) => {
-    if (webContents.getType() === 'webview') {
-        webContents.setWindowOpenHandler(({ url }) => {
-            require('electron').shell.openExternal(url)
-            return { action: 'deny' }
-        })
-    }
-
-    webContents.on('context-menu', async (e, params) => {
-        const menu = buildChromeContextMenu({
-            params,
-            webContents,
-            openLink: (url, disposition) => {
-                webContents.loadURL(url)
-            }
-        })
-
-        menu.popup()
-    })
 })
--- twitch.js
+++ twitch.js
@@ -1,8 +1,35 @@
 document.body.onload = () => {
     let webview = document.getElementById("myWebview")
-    console.log(webview)
     let title = document.getElementById("window-title-text")
-    webview.addEventListener("load-commit", () => {
+    const removeCarousel = function () {
+        function clearInner(node) {
+            while (node.hasChildNodes()) {
+                clear(node.firstChild);
+            }
+        }
+
+        function clear(node) {
+            while (node.hasChildNodes()) {
+                clear(node.firstChild);
+            }
+            node.parentNode.removeChild(node);
+            console.log(node, "cleared!");
+        }
+
+        let carousel = document.getElementsByClassName("front-page-carousel");
+        clearInner(carousel?.item(0));
+        carousel?.item(0)?.remove();
+
+        let bits = document.body.querySelector("button[data-a-target='top-nav-get-bits-button']")?.parentElement;
+        clearInner(bits);
+        bits?.remove();
+    }.toString().slice(14, -3)
+
+    webview.addEventListener("page-title-updated", () => {
         title.innerText = webview.getTitle()
+    })
+
+    webview.addEventListener("dom-ready", () => {
+        webview.executeJavaScript(removeCarousel)
     })
 }
